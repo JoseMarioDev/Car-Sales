e7e87df1d3313d7884d2a98a7cb27d8c2fbeef93
Jose Montero Jr <josemariodev@gmail.com>
2019-07-25 11:16:19 -0500

    coded features, able to add feature

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -4,8 +4,8 @@ const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button className='button'>X</button>
+      {props.feature}
     </li>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { reducer } from './store/reducers/index';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
-
-function reducer() {
-  return {
-    title: "Hello world! I'm in the Redux store!",
-  };
-}
 
 const store = createStore(reducer);
 
